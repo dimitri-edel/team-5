33cc385880c99fe3692518cbc18c77a18de72de2
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-02-12 21:38:56 +0000

    Initial frontend commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { HelloWorld } from './components/HelloWorld'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Team 5 API Demo</h1>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
