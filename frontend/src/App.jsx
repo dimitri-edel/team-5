9b2a9ca690cfaf00f415022449f7619934f9d6ae
Nazek Almalaika Altayeb <n.almalaika@yahoo.com>
2025-02-14 10:12:35 +0100

    re-structure the frontend + update App.jsx + update main.jsx + install all necessary libraries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { HelloWorld } from './components/HelloWorld'
+import styles from './App.module.css';
+import NavBar from 'react-bootstrap';
+import Container from 'react-bootstrap';
+import Route from 'react-router-dom';
+import Switch from 'react-switch';
+import signUp from './pages/registration/signUp';
+import signIn from './pages/registration/signIn';
 import './App.css'
-
 function App() {
-  return (
-    <div className="app">
-      <h1>Team 5 API Demo</h1>
-      <HelloWorld />
-    </div>
-  )
+  return (
+    <div className={styles.App}>
+       <NavBar/>
+        <Container className={styles.Main}>
+            <Switch>
+                <Route exact path='/signIn' render={() => <signIn />} />
+                <Route exact path='/signUp' render={() => <signUp />} />       
+            </Switch>
+        </Container>
+        
+    </div>
+    /*<div className="app">
+      <h1>Team 5 API Demo</h1>
+      <HelloWorld />
+    </div>*/
+);
 }
-
-export default App 
+export default App;
