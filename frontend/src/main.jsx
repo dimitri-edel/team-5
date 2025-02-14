8f22d6ef381e0891b7c736aba8201a20b855b488
Nazek Almalaika Altayeb <n.almalaika@yahoo.com>
2025-02-14 10:25:29 +0100

    install necessary packages + update App.jsx + update main.jsx + update App.module.css

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+
+import './index.css'
+import App from './App.jsx'
+ReactDOM.render(
+  <React.StrictMode>
+      <Router>
+         <App />
+      </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
