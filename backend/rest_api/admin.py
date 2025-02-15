21fc41c67897463dab88a2c1df0c3820e5bb731c
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 14:56:08 +0100

    Restructure the repository folder structure

---
+++ backend/rest_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# No models to register for Hello World endpoint
+
---
+++ backend/rest_api/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
