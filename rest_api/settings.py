dc72a0a2641d3618f3e401e252f4703e7d81a305
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-02-15 12:21:51 +0000

    chore: update database configuration to use PostgreSQL environment variables

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -119,10 +119,14 @@ WSGI_APPLICATION = 'rest_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
