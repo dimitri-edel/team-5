3237218e7620824ee7da7e14350b303a159588d7
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 22:38:28 +0100

    rectify a typo in navbar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import styles from "./App.module.css";
-import Container  from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/registration/SignUp";
 import SignIn from "./pages/registration/SignIn";
@@ -11,20 +11,20 @@ import "./App.css";
 
 function App() {
 
-  return (
-    <div className={styles.App}>
-       <NavBar/>
-        <Container className={styles.Main}>
-            <Routes>
-                <Route path="/SignIn" element={<SignIn />} />
-                <Route path="/SignUp" element={<SignUp />} />
-                <Route path="/AboutPage" element={<AboutPage />} />
-            </Routes>   
-        </Container>
-        
-    </div>
- 
-);
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+                <Routes>
+                    <Route path="/SignIn" element={<SignIn />} />
+                    <Route path="/SignUp" element={<SignUp />} />
+                    <Route path="/AboutPage" element={<AboutPage />} />
+                </Routes>
+            </Container>
+
+        </div>
+
+    );
 }
 
 
