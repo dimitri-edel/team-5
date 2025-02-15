d31ff89a7eabdb5d2fa0ea7f83553065bfceec04
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 22:06:56 +0100

    change settings.py
    add settings for rest_framwork to return json only

--- backend/rest_api/views.py
+++ backend/rest_api/views.py
@@ -3,5 +3,5 @@ from rest_framework.response import Response
 
 class HelloWorld(APIView):
     def get(self, request):
-        data = {"message": "Hello, World!"}        
-        return Response(data)
+        data = {"message": "Hello, World!"}
+        return Response(data, content_type="application/json")
