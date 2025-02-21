0383183977af6df9b502bdc011d2e92f835e09c4
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-02-21 12:11:01 +0000

    migrate new backend with new supabase db configuration

---
+++ .archive/frontend/backend/rest_api/env.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ['DB_USERNAME'] = 'dating_app'
+os.environ['DB_PASSWORD'] = 'dat12dat34X'
+os.environ['DB_NAME'] = 'dating_app'
+os.environ['DB_HOST'] = '212.227.155.64'
+os.environ['DB_PORT'] = '5432'
+os.environ['SECRET_KEY'] = 'django-insecure-7!6slkdf"!@#lkjdf'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://786947881813981:-M9lTIOU9N78bYydZp-iBAiJ4yQ@dxurmegis'
+os.environ['DEBUG'] = 'True'
