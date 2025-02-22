66b5b44de19340142292c6f1923d595dfbd780f6
dimitri-edel <dimitri.edel@gmx.net>
2025-02-22 18:45:18 +0100

    Alter likes.models.Like. Add boolean field read

--- backend/likes/models.py
+++ backend/likes/models.py
@@ -4,8 +4,9 @@ from user_profile.models import UserProfile
 class Like(models.Model):
     user = models.ForeignKey(UserProfile, related_name='source_profile', on_delete=models.CASCADE)
     likes = models.ForeignKey(UserProfile, related_name='target_profile', on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ['user', 'likes']
+        unique_together = ['user', 'likes']        
         ordering = ['-created_at']
