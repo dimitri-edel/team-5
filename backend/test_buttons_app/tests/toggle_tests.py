7faa166422a2087d429c5c344a6f76f429a59738
dimitri-edel <dimitri.edel@gmx.net>
2025-02-24 22:29:56 +0100

    Create test_button_app/tests/toggle_tests.py for enabling or disabling tests in the current directory

---
+++ backend/test_buttons_app/tests/toggle_tests.py
@@ -0,0 +1,24 @@
+import os
+
+def rename_test_files():
+    # Get the current working directory
+    base_dir = os.getcwd()
+    
+    # Walk through all subdirectories
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            # If the filename begins with 'test_', insert '_' at the beginning of the filename
+            if file.startswith('test_'):
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(root, f'_{file}')
+                os.rename(old_path, new_path)
+                print(f'Renamed: {old_path} to {new_path}')
+            # Else if the filename begins with '_test_', remove '_' from the beginning of the filename
+            elif file.startswith('_test_'):
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(root, file[1:])
+                os.rename(old_path, new_path)
+                print(f'Renamed: {old_path} to {new_path}')
+
+if __name__ == "__main__":
+    rename_test_files()
